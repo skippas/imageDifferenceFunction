4df31bd85026607b521bc9179b7213eb65b1eaa8
Andre Scheepers <pav6330@gmail.com>
2025-05-02 16:54:34 -0500

    removed code that processes joschens data structure from matlab. because ultimately I need to reproduce that data on my machine

--- scripts/import_matlab_data_and_plot_tIDF.R
+++ scripts/import_matlab_data_and_plot_tIDF.R
@@ -7,139 +7,119 @@ library(ggplot2)
 library(tidyverse)
 
 process_single_idf <- function(mat_path) {
-  # Load .mat file and extract 'idf' object
   idf <- readMat(mat_path)
   idf <- idf$idf
-  # Drop singleton second dimension if present
   idf <- idf[, 1, , drop = TRUE]
-  # Transpose so transects are rows
   idf <- aperm(idf, c(2, 1))
-  # Convert to tibble
   tibble_idf <- as_tibble(idf)
   
-  # Define matrix columns to extract
+  print("Tibble columns:")
+  print(names(tibble_idf))  # <-- see what columns actually exist
+  
   matrix_cols <- c("IDF4590", "IDFAll", "IDF045", "IDFMinus450", "IDFMinus9045")
-  # Reshape and extract row 180
+  available_cols <- intersect(matrix_cols, names(tibble_idf))
+  
+  print("Available columns:")
+  print(available_cols)
+  
   tbl_long <- tibble_idf %>%
     pivot_longer(
-      cols = all_of(matrix_cols),
+      cols = all_of(available_cols),
       names_to = "slice",
       values_to = "rms_matrix"
-    ) %>%
+    )
+  
+  print("After pivot_longer:")
+  print(tbl_long)
+  
+  tbl_long <- tbl_long %>%
     mutate(
-      rms_diff = lapply(rms_matrix, function(mat) as.vector(mat[180, ])),
+      rms_diff = lapply(rms_matrix, function(mat) {
+        if (is.null(mat) || length(mat) == 0) {
+          NA  # return NA if missing
+        } else {
+          as.vector(mat[180, ])
+        }
+      }),
       distance = lapply(distance, as.vector)
-    ) %>%
+    )
+  
+  print("After mutate rms_diff and distance:")
+  glimpse(tbl_long)
+  
+  tbl_long <- tbl_long %>%
     unnest_longer(col = c(rms_diff, distance))
   
   return(tbl_long)
 }
-idf_2024 <- process_single_idf("../matlab/results/belowCanopyBCI2024.mat")
-idf_2023 <- process_single_idf("../matlab/results/jochen_results/2023Panama_below.mat")
 
-glimpse(idf_2023)
-vars_to_include <- c("rms_diff", "slice", "name", "distance")
-idf_2023 <- idf_2023 %>% select(all_of(vars_to_include))
-idf_2024 <- idf_2024 %>% select(all_of(vars_to_include))
+plot_df$transects_renamed <- recode(plot_df$name,
+                                    "below Trail 16" = "belowTrail16",
+                                    "below Trail 17" = "belowTrail17",
+                                    "below Trail 18" = "belowTrail18",
+                                    "below Trail 19" = "belowTrail19",
+                                    "below Trail 20" = "belowTrail20",
+                                    "below Trail 22" = "belowTrail22",
+                                    "16-04-2024_t1" = "160424_t1",
+                                    "16-04-2024_t2" = "160424_t2",
+                                    "16-04-2024_t3" = "160424_t3",
+                                    "15-04-2024_t4" = "150424_t4",
+                                    "15-04-2024_t3" = "150424_t3",
+                                    "15-04-2024_t2" = "150424_t2",
+                                    "15-04-2024_t1" = "150424_t1"
+)
 
-# === Combine and prepare for plotting ===
-plot_df <- rbind(idf_2023, idf_2024)
+source("scripts/functions/gradient_descent_function.R")
+# quantify catchments
 
-# Factor order for consistent plotting
-plot_df$slice <- factor(plot_df$slice, levels = c(
-  "IDFMinus9045", "IDFMinus450", "IDF045", "IDF4590", "IDFAll"
-))
+# load above and below data produced by my own scripts
+idf_above_and_below <- process_single_idf("../matlab/results/belowCanopyBCI2024.mat")
+idf_above_and_below <- idf_above_and_below %>% select(all_of(vars_to_include)) 
+idf_above_and_below$name <- unlist(idf_above_and_below$name)
 
-plot_df$name <- unlist(plot_df$name)
-plot_df$name <- factor(plot_df$name)
-glimpse(plot_df)
+glimpse(idf_above_and_below)
 
-p <- plot_df %>% filter(name != "below Trail 18") %>% # why am i removing below trail 18??
-  ggplot(aes(x = distance, y = rms_diff)) +
+idf_above_and_below %>% 
+  ggplot(aes(x = distance, y = rms_diff_smoothed)) +
   geom_line() +
   labs(
     x = "Distance (m)",
     y = "RMS Pixel Difference"
   ) +
-  theme_minimal() +
-  theme(
- #   legend.title = element_blank(),
-    strip.background = element_blank(),
-    strip.placement = "outside",
-    legend.position = "top"
-  ) +
-  facet_grid(name ~ slice, switch = "both", scales = "free", space = "free",
-             labeller = labeller(label_wrap_gen(1)))
-ggsave("plots/belowCanopyTransectsMetrop.png",
-       plot = p, width = 12, height = 8, dpi = 300)
-
-# load above canopy data
-canopy <- readMat("../matlab/results/jochen_results/2023Panama_canopy.mat")
-canopy <- canopy$idf
-canopy<- canopy[-20:-25,,-1, drop = F]
-
-# change names of structure to match the function above
-dimnames(idf_2023)
-newNames <- dimnames(canopy)[[1]]
-newNames[which(newNames %in% c("IDF45", "IDF90f", "IDF135"))] <- c("IDFMinus9045", "IDFMinus450", "IDF045")
-dimnames(canopy)[[1]] <- newNames
-
-# next up, change function so that it doesnt try to work on rows that dont exist
-
-extract_transect_data <- function(idf, source_label) {
-  n_transects <- dim(idf)[3]
-  plot_data <- list()
-  
-  for (i in 1:n_transects) {
-    tr <- idf[,,i]
-    
-    transect_name <- tr$name[[1]]
-    distance      <- as.vector(tr$distance)
-    
-    # Define slices to check
-    slice_names <- c("IDFMinus9045", "IDFMinus450", "IDF045", "IDF4590", "IDFAll")
-    
-    for (slice_name in slice_names) {
-      if (!is.null(tr[[slice_name]]) && length(tr[[slice_name]]) > 0) {
-        mat <- tr[[slice_name]]
-        rms_profile <- mat[180, ]
-        
-        df <- data.frame(
-          transect = transect_name,
-          distance = distance,
-          rms_diff = rms_profile,
-          slice = slice_name,
-          source = source_label
-        )
-        
-        plot_data[[length(plot_data) + 1]] <- df
-      }
-    }
-  }
-  
-  return(do.call(rbind, plot_data))
-}
-
-canopy <- extract_transect_data(canopy, source_label = "canopy")
-
-p <- canopy %>%
-  ggplot(aes(x = distance, y = rms_diff, color = source)) +
-  geom_line() +
-  labs(
-    x = "Distance (m)",
-    y = "RMS Pixel Difference"
-  ) +
-  theme_minimal() +
+  theme_bw() +
   theme(
     #   legend.title = element_blank(),
     strip.background = element_blank(),
     strip.placement = "outside",
     legend.position = "top"
   ) +
-  facet_grid(transect ~ slice, switch = "both", scales = "free", space = "free",
-             labeller = labeller(label_wrap_gen(1)))
-ggsave("plots/aboveCanopyTransectsMetrop.png",
-       plot = p, width = 12, height = 8, dpi = 300)
+  facet_grid(name ~ slice, switch = "both", scales = "free", space = "free")
+
+library(zoo)
+idf_above_and_below <- idf_above_and_below %>%
+  group_by(name, slice) %>%
+  arrange(distance) %>%
+  mutate(rms_diff_smoothed = rollmean(rms_diff, k = 5, fill = NA, align = "center")) %>%
+  ungroup()
+
+catchment_area_results <- idf_above_and_below %>%
+  filter(rms_diff_smoothed != "NaN") %>%
+  #filter(slice == "IDFAll") %>% # function doesnt work if you pass it slices that are NA!!
+  group_by(name, slice) %>%
+  group_modify(~ find_extremum_simple(.x, distance_col = "distance", img_diff_col = "rms_diff_smoothed", direction = "ascend")) %>%
+  ungroup()
+glimpse(idf_above_and_below)
+# why se to nw transect all slices give same catchment. bug?
+
+# 'type' of transect needs to be added for this to work!
+ggplot(catchment_area_results, aes())
+
+
+# change names of structure to match the function above
+# dimnames(idf_2023)
+# newNames <- dimnames(canopy)[[1]]
+# newNames[which(newNames %in% c("IDF45", "IDF90f", "IDF135"))] <- c("IDFMinus9045", "IDFMinus450", "IDF045")
+# dimnames(canopy)[[1]] <- newNames
 
 # Why does idf045 look higher than idfALL?
 
